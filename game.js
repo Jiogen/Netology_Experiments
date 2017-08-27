6c5d9cfd758b582ac044d7db2d6902b48d1e59c3
Jiogen <jiog3n@gmail.com>
2017-08-27 16:21:09 +0300

    Add Vector Class...The first step on the way to infinity ...

--- game.js
+++ game.js
@@ -1,2 +1,14 @@
 'use strict';
-console.log('Just test for git!@@');
+
+class Vector {
+  constructor(x = 0, y = 0) {
+    this.x = x;
+    this.y = y;
+  }
+  plus(element) {
+    return new Vector(element.x + this.x, element.y + element.y);
+  }
+  times(multiplier){
+    return new Vector(multiplier * this.x, multiplier * this.y);
+  }
+}

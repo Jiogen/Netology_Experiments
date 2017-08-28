17aa790bb92c2bc6dae069cd1bf147658891f6e3
Jiogen <jiog3n@gmail.com>
2017-08-28 07:43:48 +0300

    Actor class...Beginning

--- game.js
+++ game.js
@@ -6,9 +6,17 @@ class Vector {
     this.y = y;
   }
   plus(element) {
+    // Do not forget to finish that! (need check)
     return new Vector(element.x + this.x, element.y + element.y);
   }
   times(multiplier){
     return new Vector(multiplier * this.x, multiplier * this.y);
   }
+}
+
+class Actor {
+  consturctor(location = new Vector(0, 0), size = new Vector(1, 1), speed = new Vector(0, 0) {
+    //Do not forget finish that (need check type of argument...Maybe new function ?)
+    
+  } 
 }

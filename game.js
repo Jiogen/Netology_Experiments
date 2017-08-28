70b05c289998d62231774597d991bf76a1aeae40
Jiogen <jiog3n@gmail.com>
2017-08-28 08:17:10 +0300

    Add check function... Fix Vector.plus...

--- game.js
+++ game.js
@@ -1,13 +1,22 @@
 'use strict';
 
+// Function for check types of arguments in some functions...
+function checkType(element, checkedType) {
+  if (!(element instanceof checkedType)) {
+    throw new Error(`Переданный объект [${element}] не тип ${checkedType.name}`);
+} else {
+  return element;
+  }
+}
+
 class Vector {
   constructor(x = 0, y = 0) {
     this.x = x;
     this.y = y;
   }
   plus(element) {
-    // Do not forget to finish that! (need check)
-    return new Vector(element.x + this.x, element.y + element.y);
+    checkType(element, Vector);
+    return new Vector(element.x + this.x, element.y + this.y);
   }
   times(multiplier){
     return new Vector(multiplier * this.x, multiplier * this.y);
@@ -16,7 +25,8 @@ class Vector {
 
 class Actor {
   consturctor(location = new Vector(0, 0), size = new Vector(1, 1), speed = new Vector(0, 0) {
-    //Do not forget finish that (need check type of argument...Maybe new function ?)
-    
+    this.pos = checkType(location, Vector);
+    this.size = checkType(size, Vector);
+    this.speed = checkType(speed, Vector);
   } 
 }
